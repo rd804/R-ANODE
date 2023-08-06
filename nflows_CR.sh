80d27d49fc49e9dd55280f523207114c21cab2ee
rd804 <rd804@scarletmail.rutgers.edu>
2023-08-06 11:29:52 -0400

    anode_CR_script

--- nflows_CR.sh
+++ nflows_CR.sh
@@ -2,24 +2,38 @@
 
 #SBATCH --partition=gpu          # Partition (job queue)
 #SBATCH --requeue                 # Return job to the queue if preempted
-#SBATCH --job-name=nflows_mixture  # Assign a short name to your job
+#SBATCH --job-name=nflows_mixture_SR  # Assign a short name to your job
 #SBATCH --nodes=1                 # Number of nodes you require
 #SBATCH --ntasks=1                # Total # of tasks across all nodes
+#SBATCH --array=0-1              # Array rank
 #SBATCH --cpus-per-task=1         # Cores per task (>1 if multithread tasks)
 #SBATCH --gres=gpu:1              # Number of GPUs per node
-#SBATCH --mem=2000                # Real memory (RAM) required (MB)
-#SBATCH --time=02:00:00           # Total run time limit (HH:MM:SS)
+#SBATCH --mem=8000                # Real memory (RAM) required (MB)
+#SBATCH --time=05:00:00           # Total run time limit (HH:MM:SS)
 
 
 cd /scratch/rd804/m-anode/
 
+
 source ~/.bashrc
 conda activate manode
 
-try_=$1
-group_name=$2
-job_type=$3
+#try_=$1
+#group_name=$2
+#job_type=$3
+#n_sig=$4
+try_='1'
+group_name='test'
+job_type='CR_verify'
+n_sig=1000
 
-python scripts/nflows_CR.py --try=${try_} --epochs=100 --wandb_group ${group_name} --wandb_job_type ${job_type}
+python scripts/nflows_CR.py --try ${try_} \
+    --epochs=2 --batch_size=256 --shuffle_split \
+    --split=${SLURM_ARRAY_TASK_ID} --seed ${try_} \
+    --wandb \
+    --wandb_group ${group_name} --wandb_job_type ${job_type}_${n_sig} \
+    --wandb_run_name try_${try_}_${SLURM_ARRAY_TASK_ID} \
+    --split=${SLURM_ARRAY_TASK_ID} \
+    --n_sig ${n_sig}
 
 
--- scripts/nflows_CR.py
+++ scripts/nflows_CR.py
@@ -1,187 +1,217 @@
-# %%
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm.notebook import tqdm
 from src.nflow_utils import *
+from src.generate_data_lhc import *
+from src.utils import *
+from src.flows import *
 import os
 
-# %%
 from nflows import transforms, distributions, flows
 import torch
 import torch.nn.functional as F
-from nflows.distributions import uniform
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
+# import train_test_split
+from sklearn.model_selection import train_test_split, ShuffleSplit
 import argparse
 import wandb
-
-# %%
 import pickle
+import sys
 
-# %%
 #os.environ["CUDA_VISIBLE_DEVICES"]='2'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--n_sig',type=int , default=1000)
 parser.add_argument('--try_', type=int, default=0)
-parser.add_argument('--sig_train', type=int, default=10)
-parser.add_argument('--gaussian_dim',  default=1)
-parser.add_argument('--wandb_group', type=str, default='test')
-parser.add_argument('--wandb_job_type', type=str, default='CR')
+parser.add_argument('--epochs', type=int, default=2)
+parser.add_argument('--batch_size', type=int, default=256)
+
+parser.add_argument('--resample', action='store_true', help='if data is to resampled')
+parser.add_argument('--seed', type=int, default=22, help='seed')
+parser.add_argument('--shuffle_split', action='store_true', help='if shuffle split is used')
+parser.add_argument('--split', type=int, default=1, help='split number')
+parser.add_argument('--data_dir', type=str, default='data/lhc_co', help='data directory')
+parser.add_argument('--config_file', type=str, default='scripts/DE_MAF_model.yml', help='config file')
+
+
+parser.add_argument('--wandb', action='store_true', help='if wandb is used')
+parser.add_argument('--wandb_group', type=str, default='debugging_anode_CR')
+parser.add_argument('--wandb_job_type', type=str, default='SR')
 parser.add_argument('--wandb_run_name', type=str, default='try_')
 
 
 args = parser.parse_args()
+save_path = 'results/'+args.wandb_group+'/'\
+            +args.wandb_job_type+'/'+args.wandb_run_name+'/'
 
+if os.path.exists(f'{save_path}best_val_loss_scores.npy'):
+    print(f'already done {args.wandb_run_name}')
+    sys.exit()
 
 CUDA = True
 device = torch.device("cuda:0" if CUDA else "cpu")
 
+job_name = args.wandb_job_type
 
-# initialize wandb
-wandb.init(project="m-anode", config=args,
-           group=args.wandb_group, job_type=args.wandb_job_type)
+# initialize wandb for logging
+if args.wandb:
+    wandb.init(project="r_anode", config=args,
+            group=args.wandb_group, job_type=job_name)
 
-name = args.wandb_run_name+str(args.try_)
-wandb.run.name = name
+    name = args.wandb_run_name
+    wandb.run.name = name
 
-
-
-
-save_path = 'results/'+args.wandb_group+'/'\
-            +args.wandb_job_type+'/'+name+'/'
-
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
-
-kwargs = {'num_workers': 4, 'pin_memory': True} if CUDA else {}
-kwargs = {}
 
 print(device)
 
+SR_data, CR_data , true_w, sigma = resample_split(args.data_dir, n_sig = args.n_sig, resample_seed = args.seed,resample = args.resample)
 
-# load data
-if args.gaussian_dim == 1:
-    with open('data/data.pkl', 'rb') as f:
-        data = pickle.load(f)
+print('x_train shape', SR_data.shape)
+print('true_w', true_w)
+print('sigma', sigma)
 
-    back_mean = 0
-    sig_mean = 3
-    sig_simga = 0.5
-    back_sigma = 3
+if args.wandb:
+    wandb.config.update({'true_w': true_w, 'sigma': sigma})
 
-    wandb.config.update({'back_mean': back_mean, 'sig_mean': sig_mean,
-                            'sig_sigma': sig_simga, 'back_sigma': back_sigma})
 
-    # load background
-    with open('data/background.pkl', 'rb') as f:
-        background = pickle.load(f)
+pre_parameters = preprocess_params_fit(CR_data)
+x_train = preprocess_params_transform(CR_data, pre_parameters) 
 
+
+if not args.shuffle_split:    
+    data_train, data_val = train_test_split(x_train, test_size=0.1, random_state=args.seed)
 else:
-    with open(f'data/data_2{args.gaussian_dim}.pkl') as f:
-        data = pickle.load(f)
+    ss_data = ShuffleSplit(n_splits=20, test_size=0.5, random_state=22)
+
+    print(f'doing a shuffle split with split number {args.split}')
+
+    for i, (train_index, test_index) in enumerate(ss_data.split(x_train)):
+        if i == args.split:
+            data_train, data_val = x_train[train_index], x_train[test_index]
+            break
+
+
+
+_x_test = np.load(f'{args.data_dir}/x_test.npy')
+x_test = preprocess_params_transform(_x_test, pre_parameters)
+
+
+
+traintensor = torch.from_numpy(data_train.astype('float32')).to(device)
+valtensor = torch.from_numpy(data_val.astype('float32')).to(device)
+testtensor = torch.from_numpy(x_test.astype('float32')).to(device)
+
+print('X_train shape', traintensor.shape)
+print('X_val shape', valtensor.shape)
+print('X_test shape', testtensor.shape)
+
+for key in pre_parameters.keys():
+    pre_parameters[key] = torch.from_numpy(pre_parameters[key].astype('float32')).to(device)
+
+
+
+
+# Use the standard pytorch DataLoader
+batch_size = args.batch_size
+trainloader = torch.utils.data.DataLoader(traintensor, batch_size=batch_size, shuffle=True)
+
+test_batch_size=batch_size*5
+valloader = torch.utils.data.DataLoader(valtensor, batch_size=test_batch_size, shuffle=False)
+testloader = torch.utils.data.DataLoader(testtensor, batch_size=test_batch_size, shuffle=False)
+
+
+
+# # %%
+# define savepath
+save_path = 'results/'+args.wandb_group+'/'\
+            +job_name+'/'+args.wandb_run_name+'/'
+
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
     
-    back_mean = 0
-    sig_mean = 2
-    sig_simga = 0.25
-    
 
-     
-
-sig_train = args.sig_train
-# load test set
-x_test = data[str(sig_train)]['val']['data']
+trainloss_list=[]
+valloss_list=[]
 
 
-# split background
-background_train , background_val = train_test_split(background, test_size=0.5, random_state=22)
+model = DensityEstimator(args.config_file, eval_mode=False, device=device)
 
-# load to torch dataloaders
-background_train_tensor = torch.from_numpy(background_train.astype('float32').reshape((-1,1)))
-background_val_tensor = torch.from_numpy(background_val.astype('float32').reshape((-1,1)))
-test_tensor = torch.from_numpy(x_test.astype('float32').reshape((-1,1))).to(device)
-
-batch_size = 256
-background_train_loader = torch.utils.data.DataLoader(background_train_tensor, batch_size=batch_size, shuffle=True)
-background_val_loader = torch.utils.data.DataLoader(background_val_tensor, batch_size=batch_size*6, shuffle=False)
-test_loader = torch.utils.data.DataLoader(test_tensor, batch_size=batch_size*6, shuffle=False)
-
-
-# define normalizing flow model
-model_background=define_model(nfeatures=1,nhidden=2,hidden_size=20,
-                              embedding=None,dropout=0,nembedding=0, 
-                              device=device,tailbound=20)
-
-
-
-valloss_list_background=[]
-trainloss_list_background=[]
-optimizer = torch.optim.Adam(model_background.parameters(),lr=1e-4) #,lr=1e-4)#, lr=1e-4)
-
+##############
+# train model
 for epoch in range(args.epochs):
-#     print('\n Epoch: {}'.format(epoch))
-    trainloss= train(model_background,optimizer,background_train_loader ,noise_data=0.,noise_context=0.0
-                     ,device=device)
-    valloss=val(model_background,background_val_loader, device=device)
-#     valloss=val(model_background,background_val_loader)
-#    # print('epoch '+str(epoch)+' val loss: ',valloss)
-#     # save model each epoch
-    torch.save(model_background.state_dict(), save_path+'model_CR_'+str(epoch)+'.pt')
+    trainloss=anode(model.model,trainloader,model.optimizer,pre_parameters ,device=device, mode='train')
+    valloss=anode(model.model,valloader,model.optimizer,pre_parameters, device=device, mode='val')
 
-    valloss_list_background.append(valloss)
-    trainloss_list_background.append(trainloss)
-    wandb.log({'train_loss_background': trainloss, 'val_loss_background': valloss,
-               'epoch': epoch})
-# print('done')
+    torch.save(model.model.state_dict(), save_path+'model_CR_'+str(epoch)+'.pt')
+
+    valloss_list.append(valloss)
+    trainloss_list.append(trainloss)
+
+    print('epoch: ', epoch, 'trainloss: ', trainloss, 'valloss: ', valloss)
+
+    if np.isnan(trainloss) or np.isnan(valloss):
+        print(' nan loss ')
+        if args.wandb:
+            wandb.finish()
+        sys.exit()
+
+    if args.wandb:
+        wandb.log({'train_loss': trainloss, 'val_loss': valloss, 'epoch': epoch})
+
+trainloss_list=np.array(trainloss_list)
+valloss_list=np.array(valloss_list)
 
 
-# save loss lists
-valloss_list_background=np.array(valloss_list_background)
-trainloss_list_background=np.array(trainloss_list_background)
+np.save(save_path+'trainloss_list.npy', trainloss_list)
+np.save(save_path+'valloss_list.npy', valloss_list)
 
-np.save(save_path+'trainloss.npy', trainloss_list_background)
-np.save(save_path+'valloss.npy', valloss_list_background)
+##################
+#################
 
+#########
 # load best model
-min_epoch=np.argmin(valloss_list_background)
-print('min epoch CR: ',min_epoch)
+valloss_list=np.array(valloss_list)
+min_epoch=np.argmin(valloss_list)
+print('min epoch SR: ',min_epoch)
 
-model_background.load_state_dict(torch.load(save_path+'model_CR_'+str(min_epoch)+'.pt'))
-torch.save(model_background.state_dict(), save_path+'model_CR_best.pt')
-
-bins = np.linspace(min(x_test), max(x_test), 50)
-
-model_background.eval()
-with torch.no_grad():
-        samples_background=model_background.sample(50000)
-        density_background = np.exp(model_background.log_prob(torch.from_numpy(bins.astype('float32').reshape((-1,1))).to(device)).cpu().detach().numpy())
-
-samples_background=samples_background.cpu().detach().numpy().reshape((-1))
-
-# plot density estimation
-figure=plt.figure()
-_=plt.hist(samples_background,bins=50, density=True, histtype='step', label='nflow sample')
-_=plt.hist(background_val,bins=50, density=True, histtype='step', label='valdata')
-_=plt.hist(background_train,bins=50, density=True, histtype='step', label='traindata')
-_=plt.plot(bins,density_background,label='nflow density')
-plt.legend(loc='upper right')
-plt.savefig(save_path+'nflow_CR.png')
-
-wandb.log({'nflow_CR': wandb.Image(figure)})
-plt.close()
-
-# save scores
-model_background.eval()
-with torch.no_grad():
-    log_p = model_background.log_prob(test_tensor).cpu().detach().numpy()
-    p = np.exp(log_p)
-
-np.save(save_path+'best_val_loss_scores.npy', p)
+#min_epoch = args.epochs-1
+model.model.load_state_dict(torch.load(save_path+'model_CR_'+str(min_epoch)+'.pt'))
+torch.save(model.model.state_dict(), save_path+'model_CR_best.pt')
 
 
+# check density estimation
+model.model.eval()
 
-wandb.finish()
+test_data = inverse_transform(testtensor, pre_parameters).cpu().detach().numpy()
+label_test = test_data[:,-1]
+
+train_data = inverse_transform(traintensor, pre_parameters).cpu().detach().numpy()
+val_data = inverse_transform(valtensor, pre_parameters).cpu().detach().numpy()
+
+
+model.model.eval()
+
+x_samples_train = generate_transformed_samples(model.model, traintensor, pre_parameters, device=device).cpu().detach().numpy()
+x_samples_val = generate_transformed_samples(model.model, valtensor, pre_parameters, device=device).cpu().detach().numpy()
+
+x_samples = np.vstack((x_samples_train, x_samples_val))
+all_data = np.vstack((train_data, val_data))
+
+for i in range(5):
+    figure=plt.figure()
+    #if dims > 1:
+    plt.hist(all_data[:,i],bins=100, density=True, label=f'data for {i}', histtype='step')
+    plt.hist(x_samples[:,i],bins=100, density=True, label=f'nflow sample for {i}', histtype='step')
+    plt.title(f'NFlow vs data for {i}')
+    plt.legend(loc='upper right')
+    plt.savefig(f'{save_path}/nflow_{i}.png')
+    if args.wandb:
+        wandb.log({f'nflow_SR': wandb.Image(figure)})
+
+    plt.close()
 
 
 
-
+if args.wandb:
+    wandb.finish()
+    
